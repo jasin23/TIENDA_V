1a91cff81da2fdffd7956dd609eacabd2d8a70b5
jasin23 <jasinchavarria2315@gmail.com>
2023-11-03 11:06:20 -0600

    arreglo de errores

---
+++ src/main/resources/static/js/rutina.js
@@ -0,0 +1,25 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#blah')
+                    .attr('src', e.target.result)
+                    .height(200);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+/* La siguiente funciÃ³n se utiliza para activar la cantidad de elementos seleccionados
+ * En el carrito de compras utilizando un llamado "ajax" */
+function addCard(formulario) {
+    var valor = formulario.elements[0].value;
+    var url = '/carrito/agregar';
+    url = url + '/' + valor;
+    $("#resultsBlock").load(url);
+}
